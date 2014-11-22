a4d99ea16c5d6751eb1792a33dbc909458ba3ac0
Plot2Code <jordan.cox@gmail.com>
2014-11-21 21:15:15 -0800

    adding inventory manager header and source file - Jordan

---
+++ InventoryManager.cpp
@@ -0,0 +1,258 @@
+﻿
+
+/** (っ◕‿◕)っ <(n_n<)
+
+Inventory manager source file by Jordan Cox
+
+Team 3 Group Project
+CIS 22C - 2014
+Jamie Johnson, Edward Lim, Nick Arduini, Efrain Esquivel, Jordan Cox, and Steven Bennett
+
+*/
+
+#include "InventoryManager.h"
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <cstring>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+#include <sstream> 
+
+using namespace std;
+
+/** (っ◕‿◕)っ <(n_n<)
+function calls other function, together they read files and creat initial data structures
+*/
+void InventoryManager::inventoryCreation(BinarySearchTree* binary_tree, AVLTree* avl_tree, HashTable<string, Card*>* hash_table)
+{
+	ifstream input_file_names;
+	string card_block;
+	vector<Card*> card_collection;
+
+	input_file_names.open("InputFileNames.txt");
+
+	if (!input_file_names)														//if statement to ensure .txt file exists
+	{
+		cout << "\n\tERROR! InputFileNames.txt was not found!\n\n";
+
+		exit(EXIT_FAILURE);
+	}
+
+	while (getline(input_file_names, card_block))
+	{
+		makeVector(card_collection, card_block);		//read file into vector
+
+		shuffleCollection(card_collection);				//shuffle vector of card pointer 
+
+		populateStructures(binary_tree, avl_tree, hash_table, card_collection);	//CreateAndPopulateStructures
+
+		checkLoadFactor(hash_table);					//check if re-hash needed
+	}
+}
+
+/** (っ◕‿◕)っ <(n_n<)
+read from files to make a vector of card pointers
+*/
+void InventoryManager::makeVector(vector<Card*> &card_collection, string card_block)
+{
+	ifstream block_source;
+	string temp_code, block_name, temp_buff, code, name, cost, rarity;
+	stringstream card_stats;
+
+	block_source.open(card_block);
+
+	getline(block_source, temp_buff);
+
+	block_name = temp_buff.substr(0, 2);
+
+	while (getline(block_source, temp_buff))							//read document and assign card
+	{
+		card_stats << temp_buff;
+
+		getline(card_stats, code, '\t');								//puts card number into string
+
+		code.insert(0, card_block);										//isnerts block code before card number
+
+		getline(card_stats, name, '\t');								//puts card name into string
+
+		getline(card_stats, cost, '\t');								//puts card cost into string
+
+		getline(card_stats, rarity, '\t');								//puts card rarity into string
+
+		Card* new_ptr = new Card(code, name, cost, rarity);				//create new card object
+
+		card_collection.push_back(new_ptr);								//add card pointer to vector
+	}
+
+	block_source.close();
+}
+
+/** (っ◕‿◕)っ <(n_n<)
+shuffle the vector to make better BST
+*/
+void InventoryManager::shuffleCollection(vector<Card*> &card_collection)
+{
+	int swtchIndex;
+	Card* hold;
+
+	srand(time(NULL));
+
+	for (int i = 0; i < card_collection.size(); i++)					//shuffles vector
+	{
+		swtchIndex = rand() % card_collection.size();
+		hold = card_collection[i];
+		card_collection[i] = card_collection[swtchIndex];
+		card_collection[swtchIndex] = hold;
+	}
+}
+
+/** (っ◕‿◕)っ <(n_n<)
+poepulate the structures with cards (pointers)
+*/
+void InventoryManager::populateStructures(BinarySearchTree* binary_tree, AVLTree* avl_tree, HashTable<string, Card*>* hash_table, vector<Card*>& card_collection)
+{
+	//for loop repeats with size of vector and calls following functions from 
+	//team's work
+	for (int i = 0; i < card_collection.size(); i++)
+	{
+		hash_table->addEntry(card_collection[i]->getCode(), card_collection[i]);	//Insert Hash
+
+		binary_tree->insert(card_collection[i]);		//Insert binary Tree
+
+		avl_tree->insert(card_collection[i]);		//Insert avl Tree
+	}
+}
+
+/** (っ◕‿◕)っ <(n_n<)
+checks load factor of hash
+*/
+void InventoryManager::checkLoadFactor(HashTable<string, Card*>* hash_table)
+{
+	//if statment to check if load factor is greater then 75%, if yes call below
+	if (hash_table->getLoadFactor() > 0.75)
+		reformHashTable(hash_table);	//ReHash
+}
+
+/** (っ◕‿◕)っ <(n_n<)
+reform hash table using count of old tables pointers
+*/
+void InventoryManager::reformHashTable(HashTable<string, Card*>* hash_table)
+{
+	cout << "\n\n\tHash table load factor over 75%.\n\n\tRe-hashing.\n";
+
+	int big_prime_number = getHashSizePrime(hash_table->getTotalInLists());
+
+	cout << "\n\tNew hash table size:- " << big_prime_number << endl;
+
+	HashTable<string, Card*> *temp_table = new HashTable<string, Card*>(Card::oat_hash, *hash_table, big_prime_number);		//using old hash table to populate new one "re-hashing"
+
+	delete hash_table;			//important re-hashing step
+
+	hash_table = temp_table;			//important re-hashing step
+
+	cout << "\n\tRe-hash complete.\n";
+}
+
+/** (っ◕‿◕)っ <(n_n<)
+doubles the int passed into the function
+*/
+int InventoryManager::getHashSizePrime(int get_prime)
+{
+	int find_prime = ((get_prime * 2) + 1);		//start search with an odd number over twice the size of the current count
+
+	while (checkNotPrime(find_prime))			//as long as function returns true keep searching for prime number
+	{
+		find_prime += 2;						//keep searching for a prime through odd numbers
+	}
+
+	return find_prime;
+}
+
+/** (っ◕‿◕)っ <(n_n<)
+finds next prime number after int passed into function
+*/
+bool InventoryManager::checkNotPrime(int find_prime)
+{
+	for (int i = 3; i < (find_prime / 2); i += 2)
+	{
+		if (find_prime % i == 0)				//as soon as an interger leaves no remainder return true to continue search
+			return true;
+	}
+
+	return false;								//prime number found
+}
+
+/** (っ◕‿◕)っ <(n_n<)
+save the current collection of cards with user chosen name
+*/
+void InventoryManager::saveCurrentCollection(BinarySearchTree* binary_tree)
+{
+	string save_file_name;
+
+	//while statment is to help get good name (don't forget there is a ! before the function call)
+	while (!getSaveFileName(save_file_name));	//ask for name of save-too file and check availabilty of the name
+
+	makeSaveFile(binary_tree, save_file_name);//create file with user entered name and output card collection to file 
+}
+
+/** (っ◕‿◕)っ <(n_n<)
+make sure user enters good name for save file
+*/
+bool InventoryManager::getSaveFileName(string save_file_name)
+{
+	cout << "\n\n\tEnter name of save file.\n";//cout message to request name
+
+	cin >> save_file_name;
+
+	if (ifstream(save_file_name))	//check name is good
+	{
+		cout << "\n\n\tFile already exists\n";
+		return false;
+	}
+
+	return true;
+}
+
+/** (っ◕‿◕)っ <(n_n<)
+save current collection to file
+*/
+void InventoryManager::makeSaveFile(BinarySearchTree* binary_tree, string save_file_name)
+{
+	ofstream save_file;
+
+	save_file.open(save_file_name);
+
+	binary_tree->writeTreeToFile(save_file);				//save tree to user named file
+}
+
+/** (っ◕‿◕)っ <(n_n<)
+delete all stuctures to release memory
+*/
+void InventoryManager::destroyEverything(BinarySearchTree* binary_tree, AVLTree* avl_tree, HashTable<string, Card*>* hash_table)
+{
+	delete binary_tree;		//call delete binary_tree
+
+	delete avl_tree;		//call delete avl_tree
+
+	ripEmUp(hash_table);	//traverse hash_table deleting all card objects
+
+	delete hash_table;		//call delete hash_table
+}
+
+/** (っ◕‿◕)っ <(n_n<)
+delete cards
+*/
+void InventoryManager::ripEmUp(HashTable<string, Card*>* hash_table)
+{
+	vector<Card*> card_collection;
+
+	hash_table->getItems(card_collection);
+
+	for (int i = card_collection.size(); i >= card_collection.size(); i--)	//traverse hash_table deleting all card objects
+	{
+		delete card_collection[i];
+	}
+}
---
+++ InventoryManager.h
@@ -0,0 +1,48 @@
+﻿
+
+/** (っ◕‿◕)っ <(n_n<)
+
+Inventory manager header file by Jordan Cox
+
+Team 3 Group Project
+CIS 22C - 2014
+Jamie Johnson, Edward Lim, Nick Arduini, Efrain Esquivel, Jordan Cox, and Steven Bennett
+
+*/
+
+#ifndef INVENTORY_MANAGER_H
+#define INVENTORY_MANAGER_H
+
+#include "Card.h"
+#include "BinarySearchTree.h"
+#include "AVLTree.h"
+#include "HashTable.h"
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class InventoryManager
+{
+private:
+	void makeVector(vector<Card*> &, string);
+	void shuffleCollection(vector<Card*> &);
+	void populateStructures(BinarySearchTree*, AVLTree*, HashTable<string, Card*>*, vector<Card*> &);
+	void checkLoadFactor(HashTable<string, Card*>*);
+	void reformHashTable(HashTable<string, Card*>*);
+	int getHashSizePrime(int);
+	bool checkNotPrime(int);
+	void saveCurrentCollection(BinarySearchTree*);
+	void createNewHashTable(HashTable<string, Card*>*, vector<Card*> &);
+	bool getSaveFileName(string);
+	void makeSaveFile(BinarySearchTree*, string);
+	void destroyEverything(BinarySearchTree*, AVLTree*, HashTable<string, Card*>*);
+	void ripEmUp(HashTable<string, Card*>*);
+
+public:
+	void inventoryCreation(BinarySearchTree*, AVLTree*, HashTable<string, Card*>*);
+	void saveCurrentCollection(BinarySearchTree*);
+	void destroyEverything(BinarySearchTree*, AVLTree*, HashTable<string, Card*>*);
+};
+
+#endif //INVENTORY_MANAGER_H
