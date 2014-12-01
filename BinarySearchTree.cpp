8f3672f8f32e980e84088135584d5828c058b9dd
sgedwardlim <sgedwardlim@gmail.com>
2014-12-01 12:43:40 -0800

    just made some changes to checking for empty tree
    just made some changes to checking for empty tree

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -243,7 +243,7 @@ int BinarySearchTree::size() const{
  */
 bool BinarySearchTree::isEmpty() const{
     bool isEmp = false;
-    if(count == 0) isEmp = true;
+    if(rootPtr != NULL) isEmp = true;
     
     return isEmp;
 }
